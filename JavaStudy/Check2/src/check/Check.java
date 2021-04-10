42f3fcede0f328731fda170474990728a9cd5e3c
okada1996 <aaa.naoki.1130@gmail.com>
2021-04-10 13:59:29 +0900

    チェックテスト（修正）

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -2,18 +2,17 @@ package check;
 
 import constants.Constants;
 public class Check {
-   private String firstName = "岡田";
-   private String lastName = "直樹";    
+   private static String firstName = "岡";
+   private static String lastName = "直樹";    
 
-   public void printName() {
-       String name = firstName + lastName;
-       System.out.println("printNameメソッド →"+ name);       
+   public static void printName() {
+       
+       System.out.println("printNameメソッド →"+ firstName + lastName);
    }
 
 
 public static void main(String args[]){
-    Check check = new Check();
-    check.printName();
+    printName();
     Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
     RobotPet robotPet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);   
      pet.introduce();    
