b65c5a4e13235c94b7a29c770f83e57d7f9b0ccc
okada1996 <68788991+okada1996@users.noreply.github.com>
2021-06-11 13:07:54 +0900

    Add files via upload

---
+++ PHPstudy/2-8/index.php
@@ -0,0 +1,12 @@
+<?php 
+$fruits = ["appleといったら" =>"りんご", "orangeといったら" => "みかん", "peachといったら" => "もも"];
+
+foreach ($fruits as $key => $value) {
+  echo $key;
+  echo $value;
+  echo "<br>";
+
+}
+?>
+
+
